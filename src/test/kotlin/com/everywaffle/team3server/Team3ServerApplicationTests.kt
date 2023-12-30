682e6faea4e05ec0a21a70bd77ae43726bcd48de
lhw414 <dlgusdn0414@snu.ac.kr>
2023-12-30 17:53:58 +0900

    기존 파일들 ktlint formatting

--- src/test/kotlin/com/everywaffle/team3server/Team3ServerApplicationTests.kt
+++ src/test/kotlin/com/everywaffle/team3server/Team3ServerApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class Team3ServerApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
