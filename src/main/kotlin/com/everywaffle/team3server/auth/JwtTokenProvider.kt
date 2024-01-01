e168bc078ed34a88d9bf29dcb8e1f2cede489554
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-02 02:12:41 +0900

    Feat : JWT 생성 및 인증 로직 구현

---
+++ src/main/kotlin/com/everywaffle/team3server/auth/JwtTokenProvider.kt
@@ -0,0 +1,43 @@
+package com.everywaffle.team3server.auth
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTVerificationException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.time.Instant
+import java.util.Date
+
+@Component
+class JwtTokenProvider(
+    @Value("\${jwt.secret-key}") private val secretKey: String,
+) {
+    private val algorithm: Algorithm = Algorithm.HMAC256(secretKey)
+
+    fun createToken(username: String): String {
+        // JWT 생성(생성 시간, 만료 시간, username 이용)
+        try {
+            val issuedAt = Date.from(Instant.now())
+            val expiredAt = Date.from(Instant.now().plusMillis(1000 * 3600 * 100)) // 만료 시간: 100시간
+
+            return JWT.create()
+                .withClaim("username", username)
+                .withIssuedAt(issuedAt)
+                .withExpiresAt(expiredAt)
+                .sign(algorithm)
+        } catch (e: Exception) {
+            throw JwtCreationException("Failed to create JWT: ${e.message}")
+        }
+    }
+
+    fun validateToken(token: String): Boolean {
+        // JWT에 대해 유효한지 검증
+        try {
+            val verifier = JWT.require(algorithm).build()
+            verifier.verify(token)
+            return true
+        } catch (e: JWTVerificationException) {
+            throw JwtValidationException("Failed to validate JWT: ${e.message}")
+        }
+    }
+}
