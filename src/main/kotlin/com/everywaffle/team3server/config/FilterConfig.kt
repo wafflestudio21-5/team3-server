fc9127d94d859b35e1cf17739f6ff24b69e73c1f
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-02 02:13:41 +0900

    Feat : Spring JWT 인증 필터 추가

---
+++ src/main/kotlin/com/everywaffle/team3server/config/FilterConfig.kt
@@ -0,0 +1,19 @@
+package com.everywaffle.team3server.config
+
+import com.everywaffle.team3server.auth.JwtAuthenticationFilter
+import com.everywaffle.team3server.auth.JwtTokenProvider
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig(private val jwtTokenProvider: JwtTokenProvider) {
+    // JWT 인증 로직 필터에 추가, 보호할 url 패턴 따로 지정
+    @Bean
+    fun jwtFilterRegistrationBean(): FilterRegistrationBean<JwtAuthenticationFilter> {
+        val registrationBean = FilterRegistrationBean<JwtAuthenticationFilter>()
+        registrationBean.filter = JwtAuthenticationFilter(jwtTokenProvider)
+        registrationBean.addUrlPatterns("/api/*") // 보호할 URL 패턴 지정
+        return registrationBean
+    }
+}
