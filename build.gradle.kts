869573c07ecb4596d64819301417d573a095248c
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-02 02:12:06 +0900

    Refactor : 디펜던시 용도별로 리펙토링

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,14 +22,30 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
+    // Spring Boot Starters for basic setup
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // Spring Boot starter for JPA
+    implementation("org.springframework.boot:spring-boot-starter-security") // Spring Boot starter for security
+    implementation("org.springframework.boot:spring-boot-starter-web") // Spring Boot starter for web applications
+
+    // JSON Processing
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Serialize/deserialize Kotlin classes to/from JSON
+
+    // Kotlin Reflection
+    implementation("org.jetbrains.kotlin:kotlin-reflect") // For Kotlin reflection capabilities
+
+    // Database
+    runtimeOnly("com.h2database:h2") // H2 in-memory database
+
+    // Testing Dependencies
+    testImplementation("org.springframework.boot:spring-boot-starter-test") // Spring Boot testing support
+    testImplementation("org.springframework.security:spring-security-test") // Spring Security testing support
+
+    // JWT Handling
+    implementation("com.auth0:java-jwt:4.4.0") // For JWT creation and validation
+
+    // JUnit Jupiter API & Mockito Kotlin
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0") // Mockito for Kotlin
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2") // JUnit Jupiter engine
 }
 
 tasks.withType<KotlinCompile> {
