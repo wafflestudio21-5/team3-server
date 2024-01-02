f552a530b0e60228f4765b474602c290cf682e91
김건희 <kimgunhee@gimgeonhuiui-noteubug.local>
2024-01-03 02:49:41 +0900

    test : APITest에 transactional annotation 추가

--- src/test/kotlin/com/everywaffle/team3server/user/SignUpAPITest.kt
+++ src/test/kotlin/com/everywaffle/team3server/user/SignUpAPITest.kt
@@ -1,12 +1,10 @@
 package com.everywaffle.team3server.user
 
-import com.everywaffle.team3server.config.FilterConfig
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
@@ -15,10 +13,11 @@ import org.springframework.transaction.annotation.Transactional
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@Transactional
 class SignUpAPITest @Autowired constructor (
     private val mvc: MockMvc,
     private val mapper: ObjectMapper,
-){
+) {
     @Test
     fun `회원가입시에 유저 이름 이메일이 충돌할 경우 409 응답을 내려준다`() {
         mvc.perform(
@@ -35,8 +34,6 @@ class SignUpAPITest @Autowired constructor (
                 .contentType(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().`is`(200))
-
-
 
         mvc.perform(
             post("/api/signup")
