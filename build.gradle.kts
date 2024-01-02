d5b838d309a20d9820d917693edf2cb77bb834ca
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-02 21:50:31 +0900

    Fix : spring security 디펜던시 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ repositories {
 dependencies {
     // Spring Boot Starters for basic setup
     implementation("org.springframework.boot:spring-boot-starter-data-jpa") // Spring Boot starter for JPA
-    implementation("org.springframework.boot:spring-boot-starter-security") // Spring Boot starter for security
     implementation("org.springframework.boot:spring-boot-starter-web") // Spring Boot starter for web applications
 
     // JSON Processing
@@ -39,7 +38,6 @@ dependencies {
 
     // Testing Dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test") // Spring Boot testing support
-    testImplementation("org.springframework.security:spring-security-test") // Spring Security testing support
 
     // JWT Handling
     implementation("com.auth0:java-jwt:4.4.0") // For JWT creation and validation
