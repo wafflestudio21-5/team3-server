1f298bdd9b3a2159ae01495f389fdfd5affb8f5c
yknxh <nyk7535@snu.ac.kr>
2024-01-03 10:31:15 +0900

    Feat: Spring Security를 위한 클래스 추가

--- src/main/kotlin/com/everywaffle/team3server/user/detail/CustomUserDetailsService.kt
+++ src/main/kotlin/com/everywaffle/team3server/user/detail/CustomUserDetailsService.kt
@@ -1,4 +1,18 @@
 package com.everywaffle.team3server.user.detail
 
-class CustomUserDetailsService {
+import com.everywaffle.team3server.user.model.UserEntity
+import com.everywaffle.team3server.user.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+    override fun loadUserByUsername(userName: String): UserDetails {
+        val user: UserEntity = userRepository.findByUserName(userName)
+            ?: throw UsernameNotFoundException("UserName Not Found.")
+        return CustomUserDetails(user)
+    }
+
 }
