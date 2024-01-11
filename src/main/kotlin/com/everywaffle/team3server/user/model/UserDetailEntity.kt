b6886d3fc91bfa79ad205a04582ba39c20f72d80
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-11 19:53:42 +0900

    Feat : user detail entity 추가

---
+++ src/main/kotlin/com/everywaffle/team3server/user/model/UserDetailEntity.kt
@@ -0,0 +1,27 @@
+package com.everywaffle.team3server.user.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+
+@Entity(name = "user_details")
+class UserDetailEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    @Column(nullable = false)
+    val realName: String,
+    @Column(nullable = false)
+    val nickname: String,
+    @Column(nullable = false)
+    val department: String,
+    @Column(nullable = false)
+    val studentId: Int,
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    val user: UserEntity,
+)
--- src/main/kotlin/com/everywaffle/team3server/user/model/UserEntity.kt
+++ src/main/kotlin/com/everywaffle/team3server/user/model/UserEntity.kt
@@ -1,10 +1,12 @@
 package com.everywaffle.team3server.user.model
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToOne
 
 @Entity(name = "users")
 class UserEntity(
@@ -16,4 +18,6 @@ class UserEntity(
     val password: String,
     @Column(unique = true)
     val email: String,
+    @OneToOne(mappedBy = "user_details_id", cascade = [CascadeType.ALL])
+    val userDetail: UserDetailEntity? = null,
 )
