248e42d43339a99963c8c98693a65f54bcff4e3f
navgod <buzz2604@gmail.com>
2024-01-13 13:59:37 +0900

    feat: 이메일, 비밀번호 변경 로직 작성

--- src/main/kotlin/com/everywaffle/team3server/user/service/UserDetailServiceImpl.kt
+++ src/main/kotlin/com/everywaffle/team3server/user/service/UserDetailServiceImpl.kt
@@ -58,4 +58,40 @@ class UserDetailServiceImpl(
             studentId = userDetail.studentId,
         )
     }
+    @Transactional
+    override fun changePassword(userId: Long, newPassword: String): UserDetailResponse.UserDetail {
+        val user = userRepository.findById(userId).orElseThrow {
+            RuntimeException("User not found with id: $userId")
+        }
+
+        // UserEntity 객체 업데이트
+        user.password = newPassword
+        val updatedUser = userRepository.save(user)
+
+        // UserDetailEntity 찾기 및 업데이트
+        val userDetail = userDetailRepository.findByUser(updatedUser)
+            ?: throw RuntimeException("User Detail not found with id: $userId")
+
+        val savedUserDetail = userDetailRepository.save(userDetail)
+
+        return UserDetailResponse.UserDetail(
+            userId = savedUserDetail.user.userId,
+            realName = savedUserDetail.realName,
+            nickname = savedUserDetail.nickname,
+            department = savedUserDetail.department,
+            studentId = savedUserDetail.studentId,
+        )
+    }
+
+    override fun changeEmail(userId: Long, newEmail: String): UserDetailResponse.UserDetail {
+        val user = userRepository.findById(userId).orElseThrow {
+            RuntimeException("User not found with id: $userId")
+        }
+
+        // UserEntity 객체의 이메일 필드를 직접 업데이트
+        user.email = newEmail
+        userRepository.save(user)
+
+        return getUserDetail(userId)
+    }
 }
