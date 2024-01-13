3fd83d8edf4f8164dcd36b805f339896dab803a7
navgod <buzz2604@gmail.com>
2024-01-13 14:53:36 +0900

    Feat: API 작성

--- src/main/kotlin/com/everywaffle/team3server/user/controller/UserDetailController.kt
+++ src/main/kotlin/com/everywaffle/team3server/user/controller/UserDetailController.kt
@@ -10,13 +10,7 @@ import com.everywaffle.team3server.user.service.UserDetailService
 import com.everywaffle.team3server.user.service.UserException
 import com.everywaffle.team3server.user.service.UserNotFoundException
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/details")
@@ -37,7 +31,23 @@ class UserDetailController(private val userDetailService: UserDetailService) {
         val userDetail = userDetailService.getUserDetail(id)
         return ResponseEntity.ok(userDetail)
     }
+    @PostMapping("/{id}/change-password")
+    fun changePassword(
+        @PathVariable id: Long,
+        @RequestParam newPassword: String
+    ): ResponseEntity<Unit> {
+        userDetailService.changePassword(id, newPassword)
+        return ResponseEntity.ok().build()
+    }
 
+    @PostMapping("/{id}/change-email")
+    fun changeEmail(
+        @PathVariable id: Long,
+        @RequestParam newEmail: String
+    ): ResponseEntity<Unit> {
+        userDetailService.changeEmail(id, newEmail)
+        return ResponseEntity.ok().build()
+    }
     @ExceptionHandler
     fun handleException(e: UserException): ResponseEntity<Unit> {
         val status =
