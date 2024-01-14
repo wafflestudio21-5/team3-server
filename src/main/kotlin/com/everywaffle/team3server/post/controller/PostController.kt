c7291974b92b58cf7acd210996f97fe5191198b6
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-14 15:46:43 +0900

    Feat : 글 작성, 조회, 수정, 삭제를 위한 컨트롤러 구현

--- src/main/kotlin/com/everywaffle/team3server/post/controller/PostController.kt
+++ src/main/kotlin/com/everywaffle/team3server/post/controller/PostController.kt
@@ -1,5 +1,65 @@
 package com.everywaffle.team3server.post.controller
 
-class PostController {
-    // TODO
+import com.everywaffle.team3server.post.dto.PostRequest
+import com.everywaffle.team3server.post.dto.PostResponse
+import com.everywaffle.team3server.post.service.PostException
+import com.everywaffle.team3server.post.service.PostNotFoundException
+import com.everywaffle.team3server.post.service.PostService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/posts")
+class PostController(private val postService: PostService) {
+    @PostMapping
+    fun createPost(
+        @RequestBody request: PostRequest.CreateOrUpdatePost,
+    ): ResponseEntity<PostResponse.PostDetail> {
+        val post = postService.createPost(request)
+        return ResponseEntity.ok(post)
+    }
+
+    @PutMapping("/{postId}")
+    fun updatePost(
+        @PathVariable postId: Long,
+        @RequestBody request: PostRequest.CreateOrUpdatePost,
+    ): ResponseEntity<PostResponse.PostDetail> {
+        val updatedPost = postService.updatePost(postId, request)
+        return ResponseEntity.ok(updatedPost)
+    }
+
+    @DeleteMapping("/{postId}")
+    fun deletePost(
+        @PathVariable postId: Long,
+    ): ResponseEntity<Void> {
+        postService.deletePost(postId)
+        return ResponseEntity.noContent().build()
+    }
+
+    @GetMapping("/{postId}")
+    fun getPost(
+        @PathVariable postId: Long,
+    ): ResponseEntity<PostResponse.PostDetail> {
+        val post = postService.getPostById(postId) ?: throw PostNotFoundException(postId)
+        return ResponseEntity.ok(post)
+    }
+
+    @ExceptionHandler
+    fun handlePostException(e: PostException): ResponseEntity<Unit> {
+        val status =
+            when (e) {
+                is PostNotFoundException -> 404
+                // 다른 PostException 서브클래스에 대한 처리를 여기에 추가할 수 있습니다.
+                else -> 404 // 기본적으로 처리되지 않은 예외에 대한 상태 코드
+            }
+        return ResponseEntity.status(status).build()
+    }
 }
