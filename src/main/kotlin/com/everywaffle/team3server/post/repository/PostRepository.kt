5d6d4e1a1e272a724a99a3911cfd65cfd7ac18e9
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-16 15:24:56 +0900

    Feat : home page에서 사용할 repository 쿼리 추가

--- src/main/kotlin/com/everywaffle/team3server/post/repository/PostRepository.kt
+++ src/main/kotlin/com/everywaffle/team3server/post/repository/PostRepository.kt
@@ -1,6 +1,11 @@
 package com.everywaffle.team3server.post.repository
 
+import com.everywaffle.team3server.post.model.Category
 import com.everywaffle.team3server.post.model.PostEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository : JpaRepository<PostEntity, Long>
+interface PostRepository : JpaRepository<PostEntity, Long> {
+    fun findTopByCategoryOrderByCreatedAtDesc(category: Category): PostEntity?
+
+    fun findTop2ByLikesGreaterThanOrderByCreatedAtDesc(likesThreshold: Int): List<PostEntity>
+}
