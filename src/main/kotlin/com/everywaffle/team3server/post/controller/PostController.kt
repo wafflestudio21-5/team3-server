ce2a3308812e3e58f17c9d2b35b2c5fd836da159
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-16 11:57:10 +0900

    Feat : url 변경

--- src/main/kotlin/com/everywaffle/team3server/post/controller/PostController.kt
+++ src/main/kotlin/com/everywaffle/team3server/post/controller/PostController.kt
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/post")
 class PostController(private val postService: PostService) {
     @PostMapping
     fun createPost(
