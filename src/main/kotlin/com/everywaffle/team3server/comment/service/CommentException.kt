7f0351e51bf683aa3c7b0f5e3945fec5f18f8a6c
navgod <buzz2604@gmail.com>
2024-01-17 02:59:10 +0900

    feat: exception 추가

--- src/main/kotlin/com/everywaffle/team3server/comment/service/CommentException.kt
+++ src/main/kotlin/com/everywaffle/team3server/comment/service/CommentException.kt
@@ -1,5 +1,8 @@
 package com.everywaffle.team3server.comment.service
 
+import com.everywaffle.team3server.post.service.PostException
+
 sealed class CommentException(message: String) : RuntimeException(message)
 
 class CommentNotFoundException(commentId: Long) : CommentException("Comment not found with id: $commentId")
+class CommentNeverLikedException(commentId: Long) : CommentException("Comment never liked: $commentId")
--- src/main/kotlin/com/everywaffle/team3server/post/service/PostException.kt
+++ src/main/kotlin/com/everywaffle/team3server/post/service/PostException.kt
@@ -4,6 +4,8 @@ sealed class PostException(message: String) : RuntimeException(message)
 
 class PostNotFoundException(postId: Long) : PostException("Post not found with id: $postId")
 
+class PostNeverLikedException(postId: Long) : PostException("Post never liked: $postId")
+class PostNeverScrapedException(postId: Long) : PostException("Post never scraped: $postId")
 class UnauthorizedPostAccessException(userId: Long, postId: Long) : PostException(
     "User with id: $userId is not authorized to access post with id: $postId",
 )
