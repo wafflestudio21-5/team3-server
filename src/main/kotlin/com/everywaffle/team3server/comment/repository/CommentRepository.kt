4223a40d5d0ec78f1febef5d576b242f8796447b
lhw414 <dlgusdn0414@snu.ac.kr>
2024-01-20 20:28:12 +0900

    Feat : scrap과 댓글 수 카운트 쿼리 추가

--- src/main/kotlin/com/everywaffle/team3server/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/everywaffle/team3server/comment/repository/CommentRepository.kt
@@ -1,6 +1,7 @@
 package com.everywaffle.team3server.comment.repository
 
 import com.everywaffle.team3server.comment.model.CommentEntity
+import com.everywaffle.team3server.post.model.PostEntity
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.jpa.repository.JpaRepository
@@ -10,12 +11,13 @@ import org.springframework.transaction.annotation.Transactional
 
 interface CommentRepository : JpaRepository<CommentEntity, Long> {
     fun findByPostPostIdOrderByCreatedAt(postId: Long): List<CommentEntity>
+
     @Modifying
     @Transactional
     @Query(
         """
         UPDATE comments c SET c.likes = c.likes + 1 WHERE c.commentId = :commentId
-    """
+    """,
     )
     fun incrementLikes(commentId: Long)
 
@@ -24,10 +26,15 @@ interface CommentRepository : JpaRepository<CommentEntity, Long> {
     @Query(
         """
         UPDATE comments c SET c.likes = c.likes - 1 WHERE c.commentId = :commentId
-    """
+    """,
     )
     fun decrementLikes(commentId: Long)
 
     @Query("select c from comments c where c.user.userId = :userId")
-    fun findByUserId(userId: Long, pageable: PageRequest): Page<CommentEntity>
+    fun findByUserId(
+        userId: Long,
+        pageable: PageRequest,
+    ): Page<CommentEntity>
+
+    fun countByPost(post: PostEntity): Int
 }
